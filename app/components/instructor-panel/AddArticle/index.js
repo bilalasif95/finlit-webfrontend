9018731e79021b14d9cc29e6c8c812b277dcc0de
inayat <inayat@rnssol.com>
2021-09-16 17:40:00 +0500

    Done html changes add article page

--- app/components/instructor-panel/AddArticle/index.js
+++ app/components/instructor-panel/AddArticle/index.js
@@ -126,98 +126,14 @@ function AddArticle() {
           </Row>
         </div>
       </div>
-      <div className="add_forms">
-        <h4>
-          <FormattedMessage {...messages.Lecture} />
-        </h4>
-        <p>
-          <FormattedMessage {...messages.AllFields} />
-        </p>
-        <div className="add_form">
-          <Row>
-            <Col lg={4} md={6} sm={6} xs={12}>
-              <FormGroup>
-                <Label for="lec_title">
-                  <FormattedMessage {...messages.LectureTitle} />
-                </Label>
-                <Input
-                  type="text"
-                  name="title"
-                  id="lec_title"
-                  placeholder="Lecture title"
-                />
-              </FormGroup>
-            </Col>
-            <Col lg={4} md={6} sm={6} xs={12}>
-              <FormGroup>
-                <Label for="sub_title">
-                  <FormattedMessage {...messages.SubTitle} />
-                </Label>
-                <Input
-                  type="text"
-                  name="title"
-                  id="sub_title"
-                  placeholder="Sub title"
-                />
-              </FormGroup>
-            </Col>
-            <Col lg={4} md={6} sm={6} xs={12}>
-              <FormGroup>
-                <Label for="uploadvideo">
-                  <FormattedMessage {...messages.LectureVideo} />
-                </Label>
-                <div className="camera">
-                  <div className="form-control">
-                    <p>Upload Video</p>
-                    <div className="input--file">
-                      <span>
-                        <FiCamera />
-                      </span>
-                      <input
-                        type="file"
-                        name="uploadvideo"
-                        id="uploadvideo"
-                        placeholder="Upload Video"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </FormGroup>
-            </Col>
-            <Col lg={12} md={12} sm={12} xs={12}>
-              <FormGroup>
-                <Label for="description">
-                  <FormattedMessage {...messages.Description} />
-                </Label>
-                <JoditEditor
-                  ref={editor}
-                  value={content}
-                  config={config}
-                  tabIndex={0} // tabIndex of textarea
-                  onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-                />
-                {/* onChange={newContent => { }} */}
-              </FormGroup>
-            </Col>
-          </Row>
-          <div className="form_footer">
-            <div className="top_btns">
-              <Button className="btn_add">
-                <FormattedMessage {...messages.Add} />
-              </Button>
-              <Button className="btn_del">
-                <FormattedMessage {...messages.Delete} />
-              </Button>
-            </div>
-            <div className="bottom_btns">
-              <Button className="btn_save">
-                <FormattedMessage {...messages.SaveAs} />
-              </Button>
-              <Button className="btn_submit">
-                <FormattedMessage {...messages.SubmitContinue} />
-              </Button>
-            </div>
-          </div>
+      <div className="form_footer">
+        <div className="bottom_btns">
+          <Button className="btn_save">
+            <FormattedMessage {...messages.SaveAs} />
+          </Button>
+          <Button className="btn_submit">
+            <FormattedMessage {...messages.SubmitContinue} />
+          </Button>
         </div>
       </div>
     </Wrapper>
