a65808edc051f9225df04d62800890f9b6d54609
rns <rizvi.ali@rnssol.com>
2021-09-17 14:23:20 +0500

    Committing Auth Fixes

--- app/containers/CreateNewPasswordPage/index.js
+++ app/containers/CreateNewPasswordPage/index.js
@@ -1,13 +1,66 @@
 /*
  * Create New Password Page
  */
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import { Button, FormGroup, Label, Input } from 'reactstrap';
 import messages from './messages';
+import * as qs from 'query-string';
+import axios from 'axios';
+import { API } from '../../config/config';
 
 export default function CreateNewPasswordPage() {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const ChangePasswordBtn = () => {
+    setError('');
+    if (password === '') {
+      setError('Please enter password');
+      return;
+    }
+    if (!/(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/.test(password)) {
+      setError('Weak Password . It must contain at least one special character , one number and one capital alphabet');
+      return;
+    }
+    if (confirmPassword === '') {
+      setError('Please enter Confirm Password');
+      return;
+    }
+    if (password !== confirmPassword) {
+      setError('Password & Confirm Password are not same');
+      return;
+    }
+    const parsed = qs.parse(location.search);
+    console.log(parsed);
+    const authHeaders = parsed
+      ? {
+        Authorization: `Bearer ${parsed.token}`,
+      }
+      : {};
+    axios
+      .post(`${API}api/auth/forgotConfirmPassword`, { password, confirmPassword }, {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          ...authHeaders,
+        },
+      })
+      .then(res => {
+        console.log(res);
+        setTimeout(() => {
+          window.location.href = '/login';
+        }, 3000)
+        setError(res.data.message);
+      })
+      .catch(err => {
+        setError(err.response && err.response.data.message);
+      });
+  }
+
+
   return (
     <div className="registration_page">
       <Helmet>
@@ -29,6 +82,7 @@ export default function CreateNewPasswordPage() {
                 name="newpassword"
                 id="password"
                 placeholder="New password"
+                onChange={(e) => { setPassword(e.target.value) }}
               />
             </FormGroup>
             <FormGroup>
@@ -40,14 +94,11 @@ export default function CreateNewPasswordPage() {
                 name="confirmpassword"
                 id="password"
                 placeholder="Confirm password"
+                onChange={(e) => { setConfirmPassword(e.target.value) }}
               />
             </FormGroup>
-            <div className="remember_forgot">
-              <p>
-                <FormattedMessage {...messages.PasswordMatch} />
-              </p>
-            </div>
-            <Button>
+            <p style={{color: 'red'}}>{error}</p>
+            <Button onClick={() => { ChangePasswordBtn() }}>
               <FormattedMessage {...messages.ResetPassword} />
             </Button>
           </div>
