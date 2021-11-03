9836fcdff9d53bd4e7fd01a5e24b2d42d16a9f03
WaqasHakeem <waqas@rnssol.com>
2021-11-03 15:11:43 +0500

    course details sidebar inprogress

--- app/components/student-panel/CourseDetails/CourseSidebar/Wrapper.js
+++ app/components/student-panel/CourseDetails/CourseSidebar/Wrapper.js
@@ -1,236 +1,295 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  border: 1px solid #d8d8d8;
-  border-radius: 0.625rem;
   margin: 30px 0 16px;
-  width: 100%;
-  .top_detail {
-    border-bottom: 1px solid #d8d8d8;
-    display: flex;
-    flex-direction: column;
-    padding: 1.5rem;
-    .prices {
-      align-items: flex-start;
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-start;
-      margin: 0 0 0.5rem;
+  .sidebar {
+    width: 100%;
+    .course_status {
       width: 100%;
-      .old_price {
+      .progress_chart {
         align-items: center;
+        border: 1px solid #d8d8d8;
+        border-radius: 0.313rem;
         display: flex;
-        justify-content: flex-start;
+        justify-content: space-between;
+        margin: 0 0 1.5rem;
+        overflow: hidden;
+        padding: 1rem 1.5rem;
+        width: 100%;
+        .progress_time {
+          width: 70%;
+          p {
+            color: #8b8b8b;
+            font-size: 0.875rem;
+            margin: 0;
+          }
+        }
+        .chart {
+          width: 30%;
+        }
       }
-      h4 {
-        color: #404041;
-        font-size: 1.5rem;
-        font-weight: 600;
-        margin: 0 0 0.5rem;
-      }
-      del {
-        color: #8b8b8b;
-        font-size: 0.875rem;
-        margin: 0 1rem 0 0;
-      }
-      span {
-        background-color: #ffd416;
-        border-radius: 0.188rem;
-        color: #404041;
-        font-size: 0.625rem;
-        font-weight: 550;
-        padding: 0.25rem 0.5rem;
-      }
-    }
-    .rating {
-      align-items: center;
-      display: flex;
-      justify-content: flex-start;
-      margin: 1rem 0;
-      width: 100%;
-      span {
-        font-size: 0.875rem;
-      }
-      p {
-        color: #484848;
-        margin: 0 0 0 0.5rem;
-      }
-      .stars {
-        margin-left: 0.5rem;
-        svg {
-          g {
-            path:last-child {
-              fill: #fbbf2e;
+      .progress_session {
+        border: 1px solid #d8d8d8;
+        border-radius: 0.313rem;
+        margin: 0 0 1.5rem;
+        padding: 1rem 1.5rem;
+        width: 100%;
+        .session {
+          align-items: center;
+          display: flex;
+          justify-content: space-between;
+          margin: 0 0 0.5rem;
+          width: 100%;
+          p {
+            color: #404041;
+            font-size: 0.875rem;
+            font-weight: 550;
+            margin: 0;
+          }
+        }
+        .progress_bar {
+          .progress {
+            height: 0.5rem;
+            width: 100%;
+            .progress-bar {
+              background-color: #f59d07;
             }
           }
         }
       }
     }
-    .enroll_now {
-      background-color: transparent;
-      border: 1px solid #707070;
-      color: #8b8b8b;
-      svg {
-        path {
-          stroke: #8b8b8b;
-        }
-      }
-    }
-    .add_cart {
-      background-color: #46a135;
-      border: 1px solid #46a135;
-      color: #fff;
-      svg {
-        path {
-          stroke: #fff;
-        }
-      }
-    }
-    button {
+    .intro {
+      display: none;
+      border: 1px solid #d8d8d8;
       border-radius: 0.313rem;
-      color: #fff;
-      font-size: 0.875rem;
-      font-weight: 550;
-      line-height: 17px;
-      font-size: 0.875rem;
-      padding: 0.85rem;
-      text-align: center;
-      text-decoration: none;
-      margin: 1rem 0 0;
-      padding: 0.85rem;
-      -webkit-touch-callout: none;
-      -webkit-user-select: none;
-      -khtml-user-select: none;
-      -moz-user-select: none;
-      -ms-user-select: none;
-      user-select: none;
-      -webkit-tap-highlight-color: transparent;
-      position: relative;
       width: 100%;
-      &:hover,
-      &:focus {
-        box-shadow: none;
-      }
-    }
-    button, span {
-      transition: 200ms;
-    }
-    
-    .text {
-      width: 100%;
-    }
-    
-    .icon {
-      position: absolute;
-      transform: translateX(0);
-      height: 1.25rem;
-      width: 1.25rem;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-    
-     svg {
-      font-size: 1.25rem;
-    }
-    button:hover .text {
-      color: transparent;
-    }
-    
-    button:hover .icon {
-      width: 90%;
-      transform: translateX(0);
-    }
-  }
-  .details {
-    padding: 1.5rem;
-    h4 {
-      color: #404041;
-      font-size: 1.25rem;
-      line-height: 24px;
-      margin: 0 0 1rem;
-    }
-    ul {
-      margin: 1rem 0 0;
-      padding: 0;
-      li {
-        align-items: center;
+      .top_detail {
+        border-bottom: 1px solid #d8d8d8;
         display: flex;
-        color: #484848;
-        font-size: 0.875rem;
-        line-height: 17px;
-        list-style: none;
-        justify-content: flex-start;
-        padding: 0 0 1rem;
-        svg {
-          font-size: 1.25rem;
-          margin: 0 1rem 0 0;
-        p {
-          margin: 0;
+        flex-direction: column;
+        padding: 1.5rem;
+        .prices {
+          align-items: flex-start;
+          display: flex;
+          flex-direction: column;
+          justify-content: flex-start;
+          margin: 0 0 0.5rem;
+          width: 100%;
+          .old_price {
+            align-items: center;
+            display: flex;
+            justify-content: flex-start;
+          }
+          h4 {
+            color: #404041;
+            font-size: 1.5rem;
+            font-weight: 600;
+            margin: 0 0 0.5rem;
+          }
+          del {
+            color: #8b8b8b;
+            font-size: 0.875rem;
+            margin: 0 1rem 0 0;
+          }
+          span {
+            background-color: #ffd416;
+            border-radius: 0.188rem;
+            color: #404041;
+            font-size: 0.313rem;
+            font-weight: 550;
+            padding: 0.25rem 0.5rem;
+          }
+        }
+        .rating {
+          align-items: center;
+          display: flex;
+          justify-content: flex-start;
+          margin: 1rem 0;
+          width: 100%;
+          span {
+            font-size: 0.875rem;
+          }
+          p {
+            color: #484848;
+            margin: 0 0 0 0.5rem;
+          }
+          .stars {
+            margin-left: 0.5rem;
+            svg {
+              g {
+                path:last-child {
+                  fill: #fbbf2e;
+                }
+              }
+            }
+          }
+        }
+        .enroll_now {
+          background-color: transparent;
+          border: 1px solid #707070;
+          color: #8b8b8b;
+          svg {
+            path {
+              stroke: #8b8b8b;
+            }
+          }
+        }
+        .add_cart {
+          background-color: #46a135;
+          border: 1px solid #46a135;
+          color: #fff;
+          svg {
+            path {
+              stroke: #fff;
+            }
+          }
+        }
+        button {
+          border-radius: 0.313rem;
+          color: #fff;
+          font-size: 0.875rem;
+          font-weight: 550;
+          line-height: 17px;
+          font-size: 0.875rem;
+          padding: 0.85rem;
+          text-align: center;
+          text-decoration: none;
+          margin: 1rem 0 0;
+          padding: 0.85rem;
+          -webkit-touch-callout: none;
+          -webkit-user-select: none;
+          -khtml-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+          -webkit-tap-highlight-color: transparent;
+          position: relative;
+          width: 100%;
+          &:hover,
+          &:focus {
+            box-shadow: none;
+          }
+        }
+        button, span {
+          transition: 200ms;
+        }
+        
+        .text {
           width: 100%;
         }
+        
+        .icon {
+          position: absolute;
+          transform: translateX(0);
+          height: 1.25rem;
+          width: 1.25rem;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+        
+        svg {
+          font-size: 1.25rem;
+        }
+        button:hover .text {
+          color: transparent;
+        }
+        
+        button:hover .icon {
+          width: 90%;
+          transform: translateX(0);
+        }
+      }
+      .details {
+        padding: 1.5rem;
+        h4 {
+          color: #404041;
+          font-size: 1.25rem;
+          line-height: 24px;
+          margin: 0 0 1rem;
+        }
+        ul {
+          margin: 1rem 0 0;
+          padding: 0;
+          li {
+            align-items: center;
+            display: flex;
+            color: #484848;
+            font-size: 0.875rem;
+            line-height: 17px;
+            list-style: none;
+            justify-content: flex-start;
+            padding: 0 0 1rem;
+            svg {
+              font-size: 1.25rem;
+              margin: 0 1rem 0 0;
+            p {
+              margin: 0;
+              width: 100%;
+            }
+          }
+        }
+        .form-group {
+          margin: 1.5rem 0 1rem;
+          position: relative;
+          label {
+            color: #484848;
+            font-size: 0.875rem;
+          }
+          .form-control {
+            border-top-right-radius: 0.25rem !important;
+            border-bottom-right-radius: 0.25rem !important;
+            padding: 0.6rem 1rem;
+            &:focus {
+              box-shadow: none;
+            }
+            &::placeholder {
+              color: #e6e6e6;
+            }
+          }
+          button {
+            background-color: #46a135;
+            border: 1px solid #46a135;
+            border-top-right-radius: 0.313rem;
+            border-bottom-right-radius: 0.313rem;
+            border-top-left-radius: 0;
+            border-bottom-left-radius: 0;
+            color: #fff;
+            font-size: 0.875rem;
+            font-weight: 550;
+            line-height: 17px;
+            padding: 0.72rem;
+            position: absolute;
+            right: 0;
+            top: 0;
+            width: 100px;
+            &:hover {
+              background-color: #46a135;
+              border: 1px solid #46a135;
+            }
+            &:focus {
+              background-color: #46a135;
+              border: 1px solid #46a135;
+              box-shadow: none;
+            }
+          }
+        }
+      }
+      @media (max-width: 991px) {
+        .details {
+          padding: 1rem;
+          .form-group {
+            margin: 0.5rem 0 0;
+          }
+        }
+      }
+      @media (max-width: 960px) and (min-width: 768px) {
+        margin: 0 0 1.5rem;
+      }
+      @media (max-width: 767px) {
+        margin: 1rem 0;
       }
     }
-    .form-group {
-      margin: 1.5rem 0 1rem;
-      position: relative;
-      label {
-        color: #484848;
-        font-size: 0.875rem;
-      }
-      .form-control {
-        border-top-right-radius: 0.25rem !important;
-        border-bottom-right-radius: 0.25rem !important;
-        padding: 0.6rem 1rem;
-        &:focus {
-          box-shadow: none;
-        }
-        &::placeholder {
-          color: #e6e6e6;
-        }
-      }
-      button {
-        background-color: #46a135;
-        border: 1px solid #46a135;
-        border-top-right-radius: 0.313rem;
-        border-bottom-right-radius: 0.313rem;
-        border-top-left-radius: 0;
-        border-bottom-left-radius: 0;
-        color: #fff;
-        font-size: 0.875rem;
-        font-weight: 550;
-        line-height: 17px;
-        padding: 0.72rem;
-        position: absolute;
-        right: 0;
-        top: 0;
-        width: 100px;
-        &:hover {
-          background-color: #46a135;
-          border: 1px solid #46a135;
-        }
-        &:focus {
-          background-color: #46a135;
-          border: 1px solid #46a135;
-          box-shadow: none;
-        }
-      }
-    }
-  }
-
-  @media (max-width: 991px) {
-    .details {
-      padding: 1rem;
-      .form-group {
-        margin: 0.5rem 0 0;
-      }
-    }
-  }
-  @media (max-width: 960px) and (min-width: 768px) {
-    margin: 0 0 1.5rem;
-  }
-  @media (max-width: 767px) {
-    margin: 1rem 0;
   }
 `;
 
--- app/components/student-panel/CourseDetails/CourseSidebar/index.js
+++ app/components/student-panel/CourseDetails/CourseSidebar/index.js
@@ -3,7 +3,7 @@
  */
 import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
-import { FormGroup, Input, Button } from 'reactstrap';
+import { FormGroup, Input, Progress, Button, Label } from 'reactstrap';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import { ToastContainer, toast } from 'react-toastify';
@@ -54,17 +54,19 @@ function CourseSidebar(props) {
 
   return (
     <Wrapper>
-      <div className="top_detail">
-        <div className="prices">
-          {/* <h4>${dataDetails && dataDetails.price}</h4>
+      <div className="sidebar">
+        <div className="intro">
+          <div className="top_detail">
+            <div className="prices">
+              {/* <h4>${dataDetails && dataDetails.price}</h4>
           <del>${dataDetails && dataDetails.price}</del> */}
-          <h4>$44.99</h4>
-          <div className="old_price">
-            <del>$100</del>
-            <span>50% off</span>
-          </div>
-        </div>
-        {/* <div className="rating">
+              <h4>$44.99</h4>
+              <div className="old_price">
+                <del>$100</del>
+                <span>50% off</span>
+              </div>
+            </div>
+            {/* <div className="rating">
           <p>4.7</p>
           <div className="stars">
             <RiStarSFill />
@@ -75,53 +77,53 @@ function CourseSidebar(props) {
           </div>
           <p>(76,876 ratings)</p>
         </div> */}
-        {/* <Button onClick={() => handleAddToCart()} disabled={loader}>
+            {/* <Button onClick={() => handleAddToCart()} disabled={loader}>
           {loader ? 'Loading..' : <FormattedMessage {...messages.AddtoCart} />}
         </Button> */}
-        <Button className="add_cart">
-          <span className="icon"><GrCart /></span>
-          <span className="text">
-            <FormattedMessage {...messages.AddtoCart} />
-          </span>
-        </Button>
-        {/* <div className="test">
+            <Button className="add_cart">
+              <span className="icon"><GrCart /></span>
+              <span className="text">
+                <FormattedMessage {...messages.AddtoCart} />
+              </span>
+            </Button>
+            {/* <div className="test">
           <button className="noselect"><span className="text">Delete</span></button>
         </div> */}
-        <Button className="enroll_now">
-          <span className="icon"><HiOutlineArrowNarrowRight /></span>
-          <span className="text">
-            <FormattedMessage {...messages.EnrollNow} />
-          </span>
-        </Button>
-      </div>
-      <div className="details">
-        <h4>
-          <FormattedMessage {...messages.Details} />
-        </h4>
-        <ul className="list">
-          <li>
-            <GrCirclePlay />
-            <p>2 hours</p>
-          </li>
-          <li>
-            <GoDeviceCameraVideo />
-            <p>8 Lectures</p>
-          </li>
-          <li>
-            <HiUser />
-            <p>1200 Enroll</p>
-          </li>
-          <li>
-            <BiTimeFive />
-            <p>Full lifetime access</p>
-          </li>
-          <li>
-            {/* <GrCertificate /> */}
-            <FaAward />
-            <p>Certificate of completion</p>
-          </li>
-        </ul>
-        {/* <FormGroup>
+            <Button className="enroll_now">
+              <span className="icon"><HiOutlineArrowNarrowRight /></span>
+              <span className="text">
+                <FormattedMessage {...messages.EnrollNow} />
+              </span>
+            </Button>
+          </div>
+          <div className="details">
+            <h4>
+              <FormattedMessage {...messages.Details} />
+            </h4>
+            <ul className="list">
+              <li>
+                <GrCirclePlay />
+                <p>2 hours</p>
+              </li>
+              <li>
+                <GoDeviceCameraVideo />
+                <p>8 Lectures</p>
+              </li>
+              <li>
+                <HiUser />
+                <p>1200 Enroll</p>
+              </li>
+              <li>
+                <BiTimeFive />
+                <p>Full lifetime access</p>
+              </li>
+              <li>
+                {/* <GrCertificate /> */}
+                <FaAward />
+                <p>Certificate of completion</p>
+              </li>
+            </ul>
+            {/* <FormGroup>
           <Input
             type="text"
             name="coupon"
@@ -132,6 +134,46 @@ function CourseSidebar(props) {
             <FormattedMessage {...messages.Apply} />
           </Button>
         </FormGroup> */}
+          </div>
+        </div>
+        <div className="course_status">
+          <div className="progress_chart">
+            <div className="progress_time">
+              <h4>Course Progress</h4>
+              <p>Expend time 8.3hr</p>
+            </div>
+            <div className="chart">
+              <img src="https://cdn.discordapp.com/attachments/548125906472796170/905387362706681866/unknown.png" alt="chart" height="100%" width="100%" />
+            </div>
+          </div>
+          <div className="progress_session">
+            <div className="session">
+              <p>Session 1 progress</p>
+              <p>50/100</p>
+            </div>
+            <div className="progress_bar">
+            <Progress value={50} />
+            </div>
+          </div>
+          <div className="quiz">
+            <div className="inner_box">
+              <div className="title">
+                <h4>Quiz 1</h4>
+                </div>
+                <div classname="quiz_box">
+                  <h6>The acronym PC stands for?</h6>
+                  <ul>
+                    <li>
+                      <Label>
+                        <Input type="radio" />
+                        
+                      </Label>
+                    </li>
+                  </ul>
+                  </div>
+              </div>
+            </div>
+        </div>
       </div>
       <ToastContainer />
     </Wrapper>
