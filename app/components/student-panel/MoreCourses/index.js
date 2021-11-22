ce6a909719a37675142d57885fe27a8c8393d7b7
hamzaahmed <ha3746944@gmail.com>
2021-11-22 17:34:59 +0500

    Notifications page mobile responsive done

--- app/components/student-panel/MoreCourses/index.js
+++ app/components/student-panel/MoreCourses/index.js
@@ -51,7 +51,7 @@ function MoreCourses() {
           </div>
         </div>
         <div className="col-lg-6 col-md-6 col-sm-6">
-          <div className="card">
+          <div className="card sm-mt-16">
             <img className="card-img-top" src={Card} alt="CardCap" />
             <div className="card-body">
               <h5 className="card-title">
